82d1afbcd40d6040027df26eeb55063b0e410bef
Francisco Puga <fpuga@icarto.es>
2017-02-10 11:53:22 +0100

    Layers in the ELLE add layer wizard are sort by position

--- src/es/udc/cartolab/gvsig/elle/gui/ElleWizard.java
+++ src/es/udc/cartolab/gvsig/elle/gui/ElleWizard.java
@@ -224,7 +224,9 @@ public class ElleWizard extends WizardPanel {
 					    layers = dbs.getTable(
 						    DBStructure.getMapTable(),
 						    DBStructure.getSchema(),
-						    where);
+						    where,
+						    new String[] {"posicion", "nombre_capa",
+						    true);
 				} catch (SQLException e) {
 				    JOptionPane.showMessageDialog(null,
 					    "Error SQL: " + e.getMessage(),
